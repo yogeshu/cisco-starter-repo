a2435214fd2b0785d17e1b099f5cd44e03bfa64c
Yogeshu <yogeshbhavsar1994@gmail.com>
2023-09-23 22:16:47 +0530

    Added Both IP

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,14 +1,42 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import Banner from "./components/Banner";
 import Card from "./components/Card";
 
 function App() {
+  const [ip, setIp] = useState("");
+  const [universalIp, setUniversalIp] = useState("");
+
+  const fetchIp = async () => {
+    try {
+      const response = await fetch("https://api.ipify.org?format=json");
+      const data = await response.json();
+      setIp(data.ip);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fetchUniversalIp = async () => {
+    try {
+      const response = await fetch("https://api64.ipify.org?format=json	");
+      const data = await response.json();
+      setUniversalIp(data.ip);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchIp();
+    fetchUniversalIp();
+  }, []);
+
   return (
     <>
       <Banner />
 
       <div className="cards-container">
-        <Card />
+        <Card ip={ip} universalIp={universalIp} />
       </div>
     </>
   );
--- sextant/src/components/Card.js
+++ sextant/src/components/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ ip, universalIp }) => {
   return (
     <div className="card">
       <img
@@ -9,15 +9,11 @@ const Card = () => {
         className="card-image"
       />
       <div className="card-content">
-        <h2 className="card-heading">Card Heading</h2>
-        <p className="card-description">
-          This is a description of the card. You can provide some information
-          about the card's content here.
-        </p>
+        <h2 className="card-heading">IP Creation Card</h2>
+        <h2 className="card-description">my ip is {ip}</h2>
         <ul className="card-details">
-          <li>Detail 1: Some information</li>
-          <li>Detail 2: More information</li>
-          <li>Detail 3: Additional details</li>
+          <li>My IpV4 - {ip}</li>
+          <li>MY IpV4(v6) is -{universalIp}</li>
         </ul>
         <button className="card-cta">Call to Action</button>
       </div>
